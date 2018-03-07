08c37b2b216c73251cb90586e6a50d0de25afe6f
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-03-07 11:52:25 +0000

    Kata working, refactoring needed

--- src/main/scala/HarryPotter/HarryPotter.scala
+++ src/main/scala/HarryPotter/HarryPotter.scala
@@ -14,17 +14,20 @@ case class Ingredient(name : String)
 
 
 object HarryPotter extends App {
+
+  Cauldron.brew(Ingredient("Aconite"), Ingredient("Adder's Fork"))
+  Cauldron.brew(Ingredient("Niffler's Fancy"), Ingredient("Nightshade"))
+  Cauldron.brew(Ingredient("anything else"))
 }
 
 object Cauldron {
   def brew(ing: Ingredient*): Option[Potion] = {
-    val ingList = ing.toList
-    ingList match {
+    val ingSet = ing.toSet
 
-      case List(Ingredient("Aconite"), Ingredient("Adder's Fork")) => Some(new PolyJuicePotion)
-      case List(Ingredient("Niffler's Fancy"), Ingredient("Nightshade")) => Some(new DraughtPotion)
+    ing.toList match {
+      case _@List(_,_) if ingSet == Set(Ingredient("Adder's Fork"), Ingredient("Aconite")) => Some(new PolyJuicePotion)
+      case _@List(_,_) if ingSet == Set(Ingredient("Niffler's Fancy"), Ingredient("Nightshade")) => Some(new DraughtPotion)
       case _ => None
     }
-
   }
 }
